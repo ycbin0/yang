f2743c417984c36a41eab41121e5b0794a93f8de
gis bin <98756042@qq.com>
2022-09-16 18:12:08 +0800

    2

--- microservice-feign-hystrix-fallback-stream/src/main/java/com/bin/microservicefeignhystrixfallbackstream/MicroserviceFeignHystrixFallbackStreamApplication.java
+++ microservice-feign-hystrix-fallback-stream/src/main/java/com/bin/microservicefeignhystrixfallbackstream/MicroserviceFeignHystrixFallbackStreamApplication.java
@@ -3,9 +3,11 @@ package com.bin.microservicefeignhystrixfallbackstream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 //import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 //@EnableHystrixDashboard //开启hystrix 的界面监控
+@EnableTurbine
 @EnableEurekaClient
 @SpringBootApplication
 public class MicroserviceFeignHystrixFallbackStreamApplication {
